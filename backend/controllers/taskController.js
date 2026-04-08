4b484fa941d14aabceb89011d2d164d1b61bad1a
DonaTT <116920667+dDonaTT@users.noreply.github.com>
2026-04-08 23:53:56 +0200

    Task router & controller finished

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -2,29 +2,46 @@ const Task = require("../models/Task");
 const User = require("../models/User");
 
 const getTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+const getTaskById = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
     }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(201).json(task);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
+const upateTask = async (req, res) => {};
+const getDashboardData = async (req, res) => {};
+const getUserDashboardData = async (req, res) => {};
+const updateTaskStatus = async (req, res) => {};
+const updateTaskChecklist = async (req, res) => {};
 const deleteTask = async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id);
-        if (!task) {
-            return res.status(404).json({ message: "Task not found" });
-        }
-        res.status(200).json({ message: "Task deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
     }
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
+module.exports = { getTasks, getTaskById,updateTask, getDashboardData, getUserDashboardData, updateTaskStatus, updateTaskChecklist, createTask, deleteTask };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
-const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskController");
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+  getTaskById,
+  getDashboardData,
+  getUserDashboardData,
+  updateTaskStatus,
+  updateTaskChecklist,
+} = require("../controllers/taskController");
 const router = express.Router();
 
-router.get("/", protect, adminOnly, getTasks);
+router.get("/dashboard-data", protect, getDashboardData);
+router.get("/user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks);
+router.get("/:id", protect, getTaskById);
 router.post("/", protect, adminOnly, createTask);
-router.put("/:id", protect, adminOnly, updateTask);
-router.delete("/:id", protect, adminOnly, `deleteTask`);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, adminOnly, deleteTask);
+router.put("/update-status/:id", protect, updateTaskStatus);
+router.put("/:id/todo", protect, updateTaskChecklist);
+
 module.exports = router;
